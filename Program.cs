a4bf7f718dc6ad85fc63eca128e57a9417572977
justinsorensen2 <justinsorensen2@gmail.com>
2020-02-10 17:14:54 -0500

    Learned a lot about writeline method.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Variables
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //create var for #cups of coffee I drink daily
+      var numberOfCupsOfCoffee = 4;
+      //create var for my full name
+      var fullName = "Justin Sorensen";
+      //create var for today and set to today's date
+      var today = "02/10/2020";
+      //console print the above variables on a single line
+      Console.WriteLine("My name is " + fullName + ". The date is " + today + ". I've had " + numberOfCupsOfCoffee + " cups of coffee today.");
+      //Ask user for name
+      Console.WriteLine("What is your name?");
+      //Store response in var user
+      var user = Console.ReadLine();
+      // Welcome user with their name
+      Console.WriteLine("Welcome, ", user, "!!");
+      //Ask user to enter a number store as operand 1
+      Console.WriteLine(user + ", please enter a number.");
+      var number1 = Console.ReadLine();
+      var operand1 = double.Parse(number1);
+      //Ask user to enter a second number and store as operand2
+      Console.WriteLine(user + ", please enter a second number.");
+      var number2 = Console.ReadLine();
+      var operand2 = double.Parse(number2);
+      //Add operands and save in var sum
+      var sum = operand1 + operand2;
+      //subtract and save in var difference
+      var difference = operand1 - operand2;
+      //divide and save in quotient var
+      var quotient = operand1 / operand2;
+      //multiply and save in product var
+      var product = operand1 * operand2;
+      //find remainder and save in var remainder
+      var remainder = operand1 % operand2;
+      //print results to screen in meaningful way
+      Console.WriteLine(number1, " plus ", number2, " = ", sum);
+      Console.WriteLine(number1, " minus ", number2, " = ", difference);
+      Console.WriteLine(number1, " divided by ", number2, " = ", quotient);
+      Console.WriteLine(number1, " multiplied by ", number2, " = ", product);
+      Console.WriteLine(number1, " divided by ", number2, " leaves a remainder of ", remainder);
+
+
+
+
+    }
+  }
+}
